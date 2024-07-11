1157defe283b2c46910b435fbbdbbe1c0affa2aa
hemanthbommana <117904125+hemanthbommana@users.noreply.github.com>
2024-07-11 12:38:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	let frm = document.getElementById(form1);
+	let fname = frm.elements['fname'].value;
+	let lname = frm.elements['lname'].value;
+	alert(fname + lname);
+	
 
 }
