e556a028b1970eb6ff165efce969081c89abc118
hemanthbommana <117904125+hemanthbommana@users.noreply.github.com>
2024-07-11 12:40:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getFormvalue() {
 	let frm = document.getElementById('form1');
 	let fname = frm.elements['fname'].value;
 	let lname = frm.elements['lname'].value;
-	alert(fname + lname);
+	alert(fname +" "+ lname);
 	
 
 }
